572a9be32e24fb337441cbe3e8592c19f8327bc7
ZmiyevskiyVladislav <zmiievskiy@gmail.com>
2022-05-09 14:06:20 +0300

    optimized code

--- cards.js
+++ cards.js
@@ -1,10 +1,6 @@
-//        window.addEventListener("load",function loadDoc(){
-//            var head = document.querySelector("head");
-//    head.append("<link rel='stylesheet' type='text/css' href='/style.css' />");
    var cards = document.querySelectorAll("#cards div");
 var cardsImg=[];
-console.log(cards);
-  var rubashka= "file:///C:/Users/zmiie/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/HTML/%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D0%B0/%D0%9F%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%BD%D0%B0%203/img/rubashka.png";
+  var rubashka= "img/rubashka.png";
 for(let i=0;i<cards.length;i++)
     {
         let j=i+1;
@@ -12,7 +8,8 @@ for(let i=0;i<cards.length;i++)
         let currentDiv=document.querySelector("#cards div:nth-child("+j+")");
         cardsImg[i]=currentImg.src;
          currentImg.src= rubashka;
-       
+       console.log(currentImg.src)
+        rubashka=currentImg.src;
         currentImg.addEventListener("click",function()
         {
             currentImg=document.querySelector("#cards div:nth-child("+j+") img");
@@ -28,7 +25,7 @@ for(let i=0;i<cards.length;i++)
                     setTimeout(function () {
                 currentImg.id='';
                 
-                }, 999)
+                }, 1000)
                 }
             else
                 {
@@ -45,7 +42,3 @@ for(let i=0;i<cards.length;i++)
             
         })
     }
-
-
-// window.removeEventListener("load",loadDoc)
-//})
--- palette.js
+++ palette.js
@@ -1,40 +1,22 @@
-//        window.addEventListener("load",function loadDoc(){
-//            var head = document.querySelector("head");
-//    head.append("<link rel='stylesheet' type='text/css' href='/style.css' />");
-   var canvas_trs = document.querySelectorAll("tr.canvas_trs");
-var palitra_tr = document.querySelectorAll("tr.palitra_trs");
-console.log(canvas_trs);
-console.log(canvas_trs.length);
-console.log(palitra_tr);
-console.log(palitra_tr.length);
+   var canvas_trs = document.querySelectorAll("#canvas tr");
+var palitra_tr = document.querySelectorAll("#palitra tr");
 var SelectedColorbefore;
 
 for (let i=1;i<palitra_tr.length+1;i++)
     {
         for(let j=1;j<palitra_tr[i-1].children.length+1;j++)
             {
-//                var selected = $("tr.palitra:nth-child(i)");
-               var selectedCellColor = document.querySelector(".palitra_trs:nth-child("+i+") td:nth-child("+j+")");
+               var selectedCellColor = document.querySelector("#palitra tr:nth-child("+i+") td:nth-child("+j+")");
                 selectedCellColor.style.border="";
-//               console.log(selectedCellColor.style.background);
                 selectedCellColor.addEventListener("click", function()
                 {
-                      console.log(SelectedColorbefore)
                     if( typeof SelectedColorbefore != 'undefined')
                         {
                             SelectedColorbefore.id="";
                         }
-                      selectedCellColor = document.querySelector(".palitra_trs:nth-child("+i+") td:nth-child("+j+")");
-                    console.log("i: "+i);
-                console.log("j: "+j);
-                     console.log("width: "+selectedCellColor.style.backgroundColor);
-//                    selectedCellColor.style.background="yellow";
-                    var selectedColor=selectedCellColor.style.backgroundColor;
+                      selectedCellColor = document.querySelector("#palitra tr:nth-child("+i+") td:nth-child("+j+")");
+                    var selectedColor=getComputedStyle(selectedCellColor).backgroundColor;
                     selectedCellColor.id="selected";
-                    console.log(selectedColor);
-                    console.log(selectedCellColor.style);
-                    console.log(selectedCellColor.style.backgroundColor);
-                    console.log(selectedCellColor.innerHTML);
                     SelectedColorbefore=selectedCellColor;
                 }
             )
@@ -46,16 +28,12 @@ for(let i=1;i<canvas_trs.length+1;i++)
         
         for(let j=1;j<canvas_trs[i-1].children.length+1;j++)
             {
-                let selected = document.querySelector(".canvas_trs:nth-child("+i+") td:nth-child("+j+")");
-                console.log("canvas: "+selected)
+                let selected = document.querySelector("#canvas tr:nth-child("+i+") td:nth-child("+j+")");
                 selected.addEventListener("click", function()
                 {
-                    let selected = document.querySelector(".canvas_trs:nth-child("+i+") td:nth-child("+j+")");
-                    selected.style.background= selectedCellColor.style.backgroundColor;
+                    let selected = document.querySelector("#canvas tr:nth-child("+i+") td:nth-child("+j+")");
+                    selected.style.background= getComputedStyle(selectedCellColor).backgroundColor;
                 }
              )
             }
     }
-
-// window.removeEventListener("load",loadDoc)
-//})
